b08849cac1b4ec7362a6ab61042273d354feaba7
gessdead <s1991gv@gmail.com>
2016-05-15 16:11:06 +0300

    ololo

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,9 @@ const mqpacker = require('css-mqpacker');
 const cleanss = require('gulp-cleancss');
 const sourcemaps = require('gulp-sourcemaps');
 const htmlhint = require('gulp-htmlhint');
-const livereload = require('gulp-livereload');
-const connect = require('gulp-connect');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const jsmin = require('gulp-jsmin');
+const browserSync = require('browser-sync');
+const ghPages = require('gulp-gh-pages');
 
 //проверка html кода
 gulp.task('htmlhint', function() {
@@ -24,6 +22,7 @@ gulp.task('htmlhint', function() {
       }))
     .pipe(htmlhint.reporter());
 });
+
 // Компиляция LESS
 gulp.task('less', function () {
   return gulp.src('./src/less/style.less')
@@ -35,16 +34,7 @@ gulp.task('less', function () {
     ]))
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('./src/css'))
-    //.pipe(livereload());
 });
-//склейка и минификакия скриптов
-//gulp.task('script', function(){
-//  return gulp.src('./src/js/*.js')
-//    .pipe(concat('all.js'))
-//    .pipe(jsmin())
-//    .pipe(rename('all.min.js'))
-//    .pipe(gulp.dest('./src/js'));
-//  });
 
 //собиратель в билд
 gulp.task('build', function(){
@@ -63,15 +53,13 @@ gulp.task('build', function(){
     .pipe(gulp.dest('./build/svg'));
   });
 
-//поднятие сервера
-gulp.task('connect', function() {
-  connect.server({
-    //root: 'src/html',
-    livereload: true
-    });
-});
+// деплоер на github.io
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
+  });
 
 // Вотчер 
-gulp.task('watch', ['connect'], function() {
+gulp.task('watch', function() {
     gulp.watch('src/less/**/*.less', ['less']);
     });
