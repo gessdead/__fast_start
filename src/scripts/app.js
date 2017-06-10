d106459535348e92dd9ff8b023ad33d74222fb8c
George Skripak <gessdead@hotmail.com>
2017-06-10 21:03:52 +0300

    create svghouse
    1) markup
    2) styles
    3) js
    4) ya zaebalsya

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,6 +11,27 @@ define('app', [
             console.log('require test done');
         });
     })($('.b-icon__camera'));
+    
+    (function($svgColor) {
+        if (!$svgColor) {
+            return;
+        }
+
+        //colors
+        var iconColor = '#2fa0a9';
+        var bgColor = '#fff';
+
+        var svg = document.querySelector('#houseset');
+        var iconWrap = svg.querySelectorAll('.icon-wrap');
+        iconWrap.forEach(function(item, index) {
+            item.addEventListener('click', function() {
+                var icon = this.querySelector('.icon');
+                var bg = this.querySelector('.background');
+                icon.style.fill = bgColor;
+                bg.style.fill = iconColor;
+            });
+        });
+    })($('.j-svg-color'));
 
     return {};
 });
